6a1ef45c57852f42968cd675fa836ed73b0bb649
Thomás Freire <thomas.fsr@gmail.com>
2024-10-24 00:32:44 -0300

    EDA for int and stg

--- northwind_dbt/models/staging/northwind/stg_northwind__employees.sql
+++ northwind_dbt/models/staging/northwind/stg_northwind__employees.sql
@@ -1,6 +1,22 @@
 with 
 source as (
     select * from {{ source('northwind','employees')}}
+),
+employees as (
+    select employee_id,
+    last_name,
+    first_name,
+    title,
+    title_of_courtesy,
+    birth_date,
+    hire_date,
+    "address",
+    city,
+    region,
+    postal_code,
+    country,
+    home_phone,
+    extension,
+    notes
 )
-
 select * from source
